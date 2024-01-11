047ece45dce8af671fb287ac00c280a63549ad1a
ReethaHosamani <hreethab@gmail.com>
2024-01-11 12:47:47 +0530

    ViewEncap Demo

---
+++ ViewEncapDemo/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ViewEncapDemo';
+
+  public chamdrayaan_update:string="The live streaming at 5.40 pm"
+}
---
+++ ViewEncapDemo/src/app/child.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+
+
+@NgModule{
+    selector:'ap-child',
+    templateUrl
+
+}
